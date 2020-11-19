aec621bbdf242220f87b9c53577e3a8945859ac5
Oskar Skak <skakkristiansen@gmail.com>
2020-11-19 20:26:02 +0100

    added kafka to spark-api as consumer

---
+++ spark-api/Kafka/KafkaManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace spark_api.Kafka
+{
+    public class KafkaManager
+    {
+        private BackgroundWorker worker;
+        private string topic;
+        public void InitiateSubscription(string topic)
+        {
+            worker = new BackgroundWorker();
+            this.topic = topic;
+        }
+
+        private async void worker_Subscribe(object sender, ElapsedEventArgs e)
+        {
+            var consumer = new SimpleMessageConsumer();
+            await consumer.SubscribeAsync(topic, Console.WriteLine);
+        }
+
+    }
+}
---
+++ spark-api/Kafka/SimpleMessageConsumer.cs
@@ -0,0 +1,53 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace spark_api.Kafka
+{
+    public class SimpleMessageConsumer
+    {
+        ConsumerConfig consumerConfig = new ConsumerConfig
+        {
+            GroupId = "test-consumer-group",
+            BootstrapServers = "localhost:9092"
+        };
+
+        public Task SubscribeAsync(string topic, Action<string> message)
+        {
+            using (var cons = new ConsumerBuilder<Ignore, string>(consumerConfig).Build())
+            {
+                cons.Subscribe(topic);
+                var cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) =>
+                {
+                    e.Cancel = true;
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                        try
+                        {
+                            var cr = cons.Consume(cts.Token);
+                            message(cr.Message.Value);
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine(e);
+                        }
+                }
+                catch (Exception)
+                {
+                    cons.Close();
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
+
