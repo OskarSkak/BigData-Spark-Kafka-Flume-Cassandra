2f6554074b4dfd818b7ba74e38fc1de31f7864e6
MSJ95 <mortenskovgaard95@gmail.com>
2020-12-02 15:00:27 +0100

    made a map

--- pandemic_client/src/Components/HeatMap.jsx
+++ pandemic_client/src/Components/HeatMap.jsx
@@ -1,57 +1,26 @@
-import React, { Component } from 'react'
-import './component.css';
-//import node from './diagram';
-
-import USA from '../USAmap.webp'
-import d3 from 'd3';
-import rd3 from 'react-d3-library';
-const RD3Component = rd3.Component;
+import React, { Component } from "react";
+import DisplayMapData from './d3/DisplayMapData'
+import DrawMap from './d3/DrawMap'
 
 export default class HeatMap extends Component {
     constructor(props) {
         super(props)
     
         this.state = {
-             d3:"",
+            countryResults:"",
         }
     }
-    
 
     componentDidMount = () => {
-        let node = document.createElement('div');
+        DrawMap();
+    }
 
-        var width = '960px',
-            height = '500px';
 
-        var svg = d3.select(node).append("svg")
-            .attr("width", width)
-            .attr("height", height);
-
-        var defs = svg.append("defs");
-
-        defs.append("clipPath")
-            .attr("id", "circle1")
-        .append("circle")
-            .attr("cx", 350)
-            .attr("cy", 200)
-            .attr("r", 180);
-
-        defs.append("clipPath")
-            .attr("id", "circle2")
-        .append("circle")
-            .attr("cx", 550)
-            .attr("cy", 200)
-            .attr("r", 180);
-
-        this.setState({d3: node});
-
-      }
     
     render() {
-        console.log("From: ",this.state.d3)
         return (
-            <div className='heatMap'>
-                <img src={USA}/>
+            <div className="mapviz">
+                
             </div>
         )
     }
--- pandemic_client/src/Components/NavBar.jsx
+++ pandemic_client/src/Components/NavBar.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import DisplayMapData from './d3/DisplayMapData'
 import './component.css';
 
 export default class NavBar extends Component {
@@ -6,8 +7,15 @@ export default class NavBar extends Component {
         super(props)
     
         this.state = {
-             
+            contryResults: "",
         }
+    }
+
+    componentDidMount = async() => {
+        let respons = await fetch('https://corona.lmao.ninja/v2/countries')
+        let data = await respons.json()
+        this.setState({contryResults: data});
+        console.log(data)
     }
     
     render() {
@@ -16,7 +24,7 @@ export default class NavBar extends Component {
                 <button onClick={()=> {alert('Twitter')}}>
                     Twitter
                 </button>
-                <button onClick={()=> {alert('Covid')}}>
+                <button onClick={(event) => DisplayMapData(event.target.value, event.target.innerText, this.state.contryResults)}>
                     Covid
                 </button>
                 <button onClick={()=> {alert('Correlation')}}>
