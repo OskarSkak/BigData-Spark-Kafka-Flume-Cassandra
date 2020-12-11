8a1776819d82fc41acc2b001ae03b5d156c1357b
Ulrik Sandberg <ujsandberg@gmail.com>
2020-12-12 00:38:17 +0100

    fix

--- PandemicAnalyzer/Consumer.py
+++ PandemicAnalyzer/Consumer.py
@@ -13,13 +13,12 @@ consumer = KafkaConsumer(
      group_id='twitteranalyzers',
      value_deserializer=lambda x: loads(x.decode('utf-8')))
 
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+producer = KafkaProducer(bootstrap_servers=['node-master:9092'],
                         value_serializer=lambda x: dumps(x).encode('utf-8'))
 
 for message in consumer:
     value = message.value
     sentiment = analyser.predict(value["text"]);
-    country = value["place"]
     value["sentiment"] = sentiment.__dict__
     json = dumps(value);
     data = json;
