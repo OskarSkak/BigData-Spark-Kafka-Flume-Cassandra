9366fd2329e408462052ab55ab57f95e49003bd0
Oskar Skak <skakkristiansen@gmail.com>
2020-12-14 16:32:37 +0100

    init aggregator service

---
+++ spark-streaming-java/spark.structured.streaming.aggregator/src/main/java/com/mycompany/app/SentimentAnalysisComparisonManager.java
@@ -0,0 +1,76 @@
+package com.mycompany.app;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author skakk
+ */
+public class SentimentAnalysisComparisonManager {
+    private final static String NEGATIVE_PREDICTION = "negative", POSITIVE_PREDICTION = "positive";
+    private final static String PREDICTION = "prediction", SENTIMENT = "sentiment";
+    private final static String NEGATIVE_CONFIDENCE = "negativeConfidence", POSITIVE_CONFIDENCE = "positiveConfidence";
+    private final static double NEGATIVE_CONFIDENCE_THRESHOLD = 0.65, POSITIVE_CONFIDENCE_THRESHOLD = 0.65;
+    
+    public static boolean hasNegativeSentiment(String val){
+        return val.toLowerCase().contains(NEGATIVE_PREDICTION.toLowerCase());
+    }
+    
+    public static boolean hasClearlyNegativeSentiment(String val){
+        try{
+            val = val.substring(1, val.length() -1);
+            JSONObject jObject = new JSONObject(val);
+            String prediction = jObject.getJSONObject(SENTIMENT).getString(PREDICTION);
+            boolean overallAssessment = prediction.toLowerCase().contains(NEGATIVE_PREDICTION);
+            double x = jObject.getJSONObject(SENTIMENT).getDouble(NEGATIVE_CONFIDENCE);
+            return (x > NEGATIVE_CONFIDENCE_THRESHOLD) && overallAssessment;
+        }catch(JSONException e){
+            System.err.print(Arrays.toString(e.getStackTrace()));
+        }
+        return false;
+    }
+    
+    public static int hasClearlyPositiveOrNegativeSentiment(String val){
+        Map<String, Double> confidenceCertaintyMap = extractConfidenceValues(POSITIVE_CONFIDENCE, NEGATIVE_CONFIDENCE, val);
+        if(confidenceCertaintyMap.get(POSITIVE_CONFIDENCE) >= POSITIVE_CONFIDENCE_THRESHOLD) return 1;
+        else if(confidenceCertaintyMap.get(NEGATIVE_CONFIDENCE) >= NEGATIVE_CONFIDENCE_THRESHOLD) return -1;
+        else return 0;
+    }
+    
+    private static Map<String, Double> extractConfidenceValues(String positiveIndicator, String negativeIndicator, String val){
+        String negativeConfidence = "";
+        String posititveConfidence = "";
+        
+        boolean negativeFound = false;
+        boolean positiveFound = false;
+        
+        String segments[] = val.split(",");
+        
+        for(int i = 0; i < segments.length; i++){
+            if(segments[i].contains(negativeIndicator) && !negativeFound){
+                negativeConfidence = segments[i];
+                negativeFound = true;
+            }
+            if(segments[i].contains(positiveIndicator) && !positiveFound){
+                posititveConfidence = segments[i];
+                positiveFound = true;
+            }
+        }
+        
+        Map<String, Double> res = new HashMap<>();
+        
+        if(!negativeConfidence.isBlank()) res.put(NEGATIVE_CONFIDENCE, extractConfidence(negativeConfidence));
+        if(!posititveConfidence.isBlank()) res.put(POSITIVE_CONFIDENCE, extractConfidence(posititveConfidence));
+        
+        return res;
+    }
+    
+    private static double extractConfidence(String val){
+        double doublePortion = Double.parseDouble(val.replaceAll("[^0-9]", ""));
+        return doublePortion / Math.pow(10, 16); //since val is always E15 away from 0-1
+    }
+}
---
+++ spark-streaming-java/spark.structured.streaming.aggregator/src/main/java/com/mycompany/app/spark/kafka/consumers/SentimentAnalysisResultConsumer.java
@@ -0,0 +1,71 @@
+package com.mycompany.app.spark.kafka.consumers;
+
+import com.mycompany.app.SentimentAnalysisComparisonManager;
+import com.mycompany.app.kafka.producers.SentimentAnalysisCorrelatedProducer;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.spark.*;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaInputDStream;
+import org.apache.spark.streaming.kafka010.ConsumerStrategies;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
+
+/**
+ * THIS IS NOW DEPRECATED - please don't use 
+ * @author skakk
+ */
+
+
+public class SentimentAnalysisResultConsumer {
+    Map<String, Object> kafkaParams = new HashMap<>();
+    SparkConf conf;
+    JavaStreamingContext ssc;
+    final String NEGATIVE_PREDICTION = "\"prediction\": \"Positive\"";
+    
+    public SentimentAnalysisResultConsumer(SparkConf _conf, JavaStreamingContext _ssc){
+        this.conf = _conf;
+        this.ssc = _ssc;
+    }
+    
+    public void initiate() throws InterruptedException{
+        Map<String, Object> kafkaParams = new HashMap<>();
+        kafkaParams.put("bootstrap.servers", "localhost:9092");
+        kafkaParams.put("key.deserializer", StringDeserializer.class);
+        kafkaParams.put("value.deserializer", StringDeserializer.class);
+        kafkaParams.put("group.id", "sentiment");
+        kafkaParams.put("auto.offset.reset", "latest");
+        kafkaParams.put("enable.auto.commit", false);
+        
+        Collection<String> topics = Arrays.asList("twitteranalyzed");
+        
+        JavaInputDStream<ConsumerRecord<String, String>> stream = 
+                KafkaUtils.createDirectStream(
+                        ssc, 
+                        LocationStrategies.PreferConsistent(), 
+                        ConsumerStrategies.<String, String>Subscribe(topics, kafkaParams)
+                );
+        
+        JavaDStream<String> lines = stream.map(ConsumerRecord::value);
+        
+        JavaDStream<String> negativeRecords = lines.filter(line -> {
+            return SentimentAnalysisComparisonManager.hasClearlyNegativeSentiment(line);
+        });
+        
+        negativeRecords.foreachRDD(rdd -> {
+            rdd.foreachPartition(partitionOfRecords -> {
+                SentimentAnalysisCorrelatedProducer producer = new SentimentAnalysisCorrelatedProducer(); 
+                while(partitionOfRecords.hasNext()){
+                    String nextRecord = partitionOfRecords.next();
+                    producer.sendSentimentCorrelatedEvent(nextRecord);
+                }
+                producer.close();
+            });
+        });
+    }
+}
