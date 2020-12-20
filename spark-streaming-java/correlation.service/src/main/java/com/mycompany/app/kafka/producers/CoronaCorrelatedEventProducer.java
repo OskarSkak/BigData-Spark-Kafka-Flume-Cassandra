7abdfe3e24fe3c1321e3c15527b1a5f6bd6bf857
Oskar Skak <skakkristiansen@gmail.com>
2020-12-20 13:57:08 +0100

    prettied up code a tiny bit

--- spark-streaming-java/correlation.service/src/main/java/com/mycompany/app/kafka/producers/CoronaCorrelatedEventProducer.java
+++ spark-streaming-java/correlation.service/src/main/java/com/mycompany/app/kafka/producers/CoronaCorrelatedEventProducer.java
@@ -17,16 +17,18 @@ public class CoronaCorrelatedEventProducer {
     String topicName = "corona";
     Properties kafkaParams = new Properties();
     Producer<String, String> producer;
+    static final String BOOTSTRAP_SERVERS = "node-master:9092,node1:19092,node2:29092", 
+            GROUP_ID = "group.id", PANDEMIC_GROUP = "pandemic.group.corona", AUTO_OFFSET_RESET = "auto.offset.reset", LATEST = "latest", 
+            ENABLE_AUTO_COMMIT = "enable.auto.commit";
     
     public CoronaCorrelatedEventProducer(){
         Map<String, Object> kafkaParams = new HashMap<>();
-        kafkaParams.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "node-master:9092,node1:19092,node2:29092");
+        kafkaParams.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         kafkaParams.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         kafkaParams.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        kafkaParams.put("group.id", "pandemic.group.corona");
-        kafkaParams.put("auto.offset.reset", "latest");
-        kafkaParams.put("enable.auto.commit", false);
-        
+        kafkaParams.put(GROUP_ID, PANDEMIC_GROUP);
+        kafkaParams.put(AUTO_OFFSET_RESET, LATEST);
+        kafkaParams.put(ENABLE_AUTO_COMMIT, false);
         producer = new KafkaProducer<>(kafkaParams);
     }
     
